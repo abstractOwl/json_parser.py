9455bc5f9fdc72974863051d13c0be4b121732ec
AbstractOwl <abstractOwl@users.noreply.github.com>
2015-11-20 00:17:57 -0800

    Refactored array/map parsing logic into separate functions

--- json_parse.py
+++ json_parse.py
@@ -11,55 +11,26 @@ import sys
 STRUCTURAL_CHARS = ['[', ']', '{', '}', ':', ',']
 
 def parse(json):
-    """
-    Parses a JSON string.
-    """
+    """ Parses a JSON string. """
     return read_from(tokenize(json))
 
 def tokenize(json):
-    """
-    Converts a string into tokens.
-    """
+    """ Converts a string into tokens. """
     for char in STRUCTURAL_CHARS:
         json = json.replace(char, " %s " % char)
     return re.split(r'[ |\t|\n|\r]+', json.strip())
 
 def read_from(tokens):
-    """
-    Deserializes tokens into Python objects.
-    """
+    """ Deserializes tokens into Python objects. """
     if len(tokens) == 0:
         raise SyntaxError('Unexpected EOF while reading!')
 
     token = tokens.pop(0)
 
     if token == '[':
-        lst = []
-        first = True
-
-        while tokens[0] != ']':
-            if first:
-                first = False
-            else:
-                assert tokens.pop(0) == ','
-            lst.append(read_from(tokens))
-        assert tokens.pop(0) == ']'
-        return lst
+        return read_array(tokens)
     elif token == '{':
-        obj = {}
-        first = True
-
-        while tokens[0] != '}':
-            if first:
-                first = False
-            else:
-                assert tokens.pop(0) == ','
-            key = tokens.pop(0)
-            assert tokens.pop(0) == ':'
-            obj[key] = read_from(tokens)
-        assert tokens.pop(0) == '}'
-        return obj
-
+        return read_map(tokens)
     elif token == ']':
         raise SyntaxError('Unexpected ]')
     elif token == '}':
@@ -69,10 +40,38 @@ def read_from(tokens):
     else:
         return atom(token)
 
+def read_array(tokens):
+    """ Parses tokens into an array. """
+    array = []
+    first = True
+
+    while tokens[0] != ']':
+        if first:
+            first = False
+        else:
+            assert tokens.pop(0) == ','
+        array.append(read_from(tokens))
+    assert tokens.pop(0) == ']'
+    return array
+
+def read_map(tokens):
+    """ Parses tokens into a map. """
+    obj = {}
+    first = True
+
+    while tokens[0] != '}':
+        if first:
+            first = False
+        else:
+            assert tokens.pop(0) == ','
+        key = tokens.pop(0)
+        assert tokens.pop(0) == ':'
+        obj[key] = read_from(tokens)
+    assert tokens.pop(0) == '}'
+    return obj
+
 def atom(token):
-    """
-    Parses the value of an atomic token.
-    """
+    """ Parses the value of an atomic token. """
     try:
         return int(token)
     except ValueError:
@@ -83,13 +82,7 @@ def atom(token):
                 return str(token[1:-1])
 
 def main():
-    """
-    Main loop
-    """
-    text = ""
-    for line in sys.stdin:
-        text += line
-
-    print parse(text)
+    """ Main loop """
+    print parse("".join([line for line in sys.stdin]))
 
 main()
